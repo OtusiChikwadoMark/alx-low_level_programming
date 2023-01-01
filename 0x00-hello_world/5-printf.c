2a94dc62e4b30240b5cb3052c42f7ced686baed8
OtusiChikwadoMark <shaydiih@gmail.com>
2023-01-01 15:46:53 -0800

    Corrected files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints aline using printf function
- * Return: 0 (Success)
+ * Description: main - Prints out texts to stdout.
+ * Return: 0 should the code runs successfully.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
