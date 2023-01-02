d70e7be1c5399b3a5c988d1f70608621b6ffc2d5
OtusiChikwadoMark <shaydiih@gmail.com>
2023-01-02 13:41:57 -0800

    free

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * Description: main - A program that prints all single digit
- *   * Return: 0
- *    */
+ * Description: main - A program that prints all single digit
+ * Return: 0
+ */
 
 int main(void)
 {
 	int num;
+
 	while (num < 10)
 	{
 		printf("%d", num);
