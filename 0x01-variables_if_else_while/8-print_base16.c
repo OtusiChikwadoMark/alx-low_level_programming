18003bb600823c686ad4eae1fc09c0950bdda9bc
OtusiChikwadoMark <shaydiih@gmail.com>
2023-01-02 13:51:12 -0800

    help

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- *  * Description: main -  A program that prints all the numbers of base 16.
- *   * You can only use the putchar function.
- *    * Return: 0
- *     */
+ * Description: main -  A program that prints all the numbers of base 16.
+ * You can only use the putchar function.
+ * Return: 0
+ */
 
 int main(void)
 {
 	int num = 48;
 	int lt = 'a';
+
 	while (num <= 57)
 	{
 		putchar(num);
