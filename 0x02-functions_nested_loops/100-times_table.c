eab0930b8f347e67a4458ce2ee886d88067846c8
OtusiChikwadoMark <shaydiih@gmail.com>
2023-01-03 01:30:04 -0800

    adjusted 9 and 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,35 +5,35 @@
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-int product;
+	int i;
+	int j;
+	int product;
 
-if (n <= 15 && n >= 0)
-{
-for (i = 0; i <= n; i++)
-{
-printf("0");
-for (j = 1; j <= n; j++)
-{
-product = i * j;
-if (product > 99)
-{
-printf(", %d", product);
-}
-else if (product > 9 && product <= 99)
-{
-printf(",  %d", product);
-}
-else
-{
-printf(",  %d", product);
-}
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			printf("0");
+			for (j = 1; j <= n; j++)
+			{
+				product = i * j;
+				if (product > 99)
+				{
+					printf(", %d", product);
+				}
+				else if (product > 9 && product <= 99)
+				{
+					printf(",  %d", product);
+				}
+				else
+				{
+					printf(",   %d", product);
+				}
 
-if (j == n)
-continue;
-}
-printf("\n");
-}
-}
+				if (j == n)
+					continue;
+			}
+			printf("\n");
+		}
+	}
 }
